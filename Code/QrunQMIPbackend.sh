0c79f50598fccc361f2df87e504593597f1d9aac
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2025-03-06 16:43:26 +0000

    Update QrunQMIPbackend.sh

--- Code/QrunQMIPbackend.sh
+++ Code/QrunQMIPbackend.sh
@@ -2,7 +2,7 @@
 module load qmio/hpc  qmio-tools/0.1.1-python-3.9.9 gcc/12.3.0 qiskit/1.0.2-python-3.9.9
 export QMIO_CALIBRATIONS=/opt/cesga/qmio/hpc/calibrations
 
-python -u pf.py
+python pf.py --sequence ... --axis ...  --weight ... --displacement ... --shots ...
 mkdir Output
 mv *png Output
 mv *txt Output
