b5ac5bce07abdae9e6994682056beffa651bd9c6
Daniel Conde Torres <117435891+DanielCondeTorres@users.noreply.github.com>
2025-03-07 20:47:34 +0000

    Update QrunQMIPbackend.sh

--- Code/QrunQMIPbackend.sh
+++ Code/QrunQMIPbackend.sh
@@ -2,9 +2,17 @@
 module load qmio/hpc gcc/12.3.0 qmio-tools/0.2.0-python-3.9.9 qiskit/1.2.4-python-3.9.9
 export QMIO_CALIBRATIONS=/opt/cesga/qmio/hpc/calibrations
 
-python pf.py --sequence ... --axis ...  --weight ... --displacement ... --shots ...
-mkdir Output
-mv *png Output
-mv *txt Output
-mv slurm* Output
-mv *json Output
+
+# Nombre del directorio que desearias
+OUTPUT_DIR="MiDirectorio"
+
+# Crear directorio de salida si no existe
+mkdir -p "$OUTPUT_DIR"
+
+# Ejecutar script de Python
+python pf.py --sequence ... --axis ... --weight ... --displacement ... --shots ...  --iteractions_cobyla ...
+
+mv *png "$OUTPUT_DIR" 
+mv *txt "$OUTPUT_DIR" 
+mv slurm* "$OUTPUT_DIR" 
+mv *json "$OUTPUT_DIR" 
